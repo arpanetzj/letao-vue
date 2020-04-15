2719625bc5a79ed4abc617b192378dfc6e9b3928
Yzj <1065673861@qq.com>
2020-04-15 20:14:12 +0800

    项目结构搭建完成

---
+++ vue.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  devServer: {
+    // 自动打开浏览器
+    open: true,
+    // 端口
+    port: 8888
+  },
+  css: {
+    loaderOptions: {
+      css: {},
+      postcss: {
+        plugins: [
+          require("postcss-px2rem")({
+            remUnit: 36
+          })
+        ]
+      }
+    }
+  }
+}
